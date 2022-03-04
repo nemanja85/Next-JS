a2df4cbdfad149d5cebd36c7befa6d5e087a2d76
nemanja85 <mitic@deutsche-teilkauf.de>
2022-03-04 16:15:27 +0100

    Added new Table POST and logging middleware

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,3 +1,16 @@
 import { PrismaClient } from '@prisma/client';
 
 export const prisma = new PrismaClient();
+
+// Logging Middleware
+prisma.$use(async (params, next) => {
+  const before = Date.now();
+
+  const result = await next(params);
+
+  const after = Date.now();
+
+  console.log(`Query ${params.model}.${params.action} took ${after - before}ms`);
+
+  return result;
+});
