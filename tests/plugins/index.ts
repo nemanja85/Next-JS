883b87d1898b958296060c12981d247dc3691825
nemanja85 <mitic@deutsche-teilkauf.de>
2022-03-04 14:02:58 +0100

    Refactoring code - added intelisense for typescript

--- tests/plugins/index.ts
+++ tests/plugins/index.ts
@@ -1,16 +1,16 @@
 /// <reference types="cypress" />
 
+import { prisma } from '@lib/prisma';
+
 module.exports = (on: Cypress.PluginEvents, _config: Cypress.PluginConfigOptions) => {
   on('task', {
     'db:teardown': async () => {
-      const { prisma } = require('@lib/prisma');
       await prisma.$executeRaw`DELETE FROM User;`;
       await prisma.$executeRaw`DELETE FROM sqlite_sequence WHERE name = 'User';`;
 
       return null;
     },
     'db:seed': async () => {
-      const { prisma } = require('@lib/prisma');
       return await prisma.user.create({
         data: {
           email: 'test@test.com',
