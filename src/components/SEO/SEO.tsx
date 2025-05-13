13ed6831d00b9f356ce6aac92d7af26d047faa38
Nemanja Mitic <mitic.nemanja@hotmail.com>
2025-05-13 21:44:54 +0200

    Update SEO.tsx

--- src/components/SEO/SEO.tsx
+++ src/components/SEO/SEO.tsx
@@ -16,25 +16,21 @@ const SEO = ({ title, description, keywords, noIndex = false }: SeoProps) => {
     <Head>
       <meta charSet="utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-      {noIndex && <meta name="robots" content="noindex, nofollow" />}
-      {!noIndex && <meta name="robots" content="index, follow" />}
+      {noIndex && <meta name="robots" content="noindex, nofollow"> <meta name="googlebot" content="noindex, nofollow" />}
+      {!noIndex && <meta name="robots" content="index, follow"> <meta name="googlebot" content="index, follow" />}
 
-      {/* TODO: Check this <meta name="googlebot" content="index, follow" /> */}
-
-      {/* The real MVP: https://moz.com/blog/meta-data-templates-123 */}
       <title>{`${title} | ${config.seo.title}`}</title>
       <meta name="description" content={description} />
       <meta name="keywords" content={keywords} />
       <link rel="canonical" href={`${config.url}${router.route}`} />
 
-      {/* <!-- Twitter Card data --> */}
       <meta name="twitter:card" content="summary" />
       <meta name="twitter:site" content={config.seo.twitter} />
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
       <meta name="twitter:creator" content={config.seo.twitter} />
-      <meta name="twitter:image" content={config.url} />
-      {/* <!-- Open Graph data --> */}
+      <meta name="twitter:image" content={`${config.url}images/twitter-card-default.png`} />
+      
       <meta property="og:title" content={title} />
       <meta property="og:url" content={config.url} />
       <meta property="og:image" content="/favicon.ico" />
